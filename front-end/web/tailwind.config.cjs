335d5c7682a6d610783456bb676860b5e572e00b
Pedro Castro <pgkastro@gmail.com>
2022-09-14 09:01:27 -0300

    Lading page name ADs

--- front-end/web/tailwind.config.cjs
+++ front-end/web/tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.88%, #43E7AD 37.94%, #E1D55D 42.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0,0) 0%, rgba(0, 0, 0, 0.9) 67.88%)',
       },
     },
   },
