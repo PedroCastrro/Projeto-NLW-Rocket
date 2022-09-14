07250c32395b0fe6951ce796c88b5684cfc4c38d
Pedro Castro <pgkastro@gmail.com>
2022-09-14 08:41:34 -0300

    Landing Page

--- front-end/web/tailwind.config.cjs
+++ front-end/web/tailwind.config.cjs
@@ -2,9 +2,15 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.88%, #43E7AD 37.94%, #E1D55D 42.57%)',
+      },
+    },
   },
   plugins: [],
 }
