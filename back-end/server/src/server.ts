04755969bbe9b117d3bf17aac8a7bccb30efd1f2
Pedro Castro <pgkastro@gmail.com>
2022-09-15 08:54:37 -0300

    Score Ads

--- back-end/server/src/server.ts
+++ back-end/server/src/server.ts
@@ -1,6 +1,15 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express()
+
+app.get('/games', (request, response) => {
+   return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+   return response.status(201).json([]);
+});
+
 
 app.get('/ads', (request, response) => {
    return response.json([
